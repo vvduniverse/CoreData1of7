e083d1187fefa532a83e71b32baf72607e0c0c89
Vahtee Boo <vahtee.boo@gmail.com>
2022-01-31 18:18:47 +0200

    Initial Commit

---
+++ CoreData1of7/CoreData1of7App.swift
@@ -0,0 +1,17 @@
+//
+//  CoreData1of7App.swift
+//  CoreData1of7
+//
+//  Created by Vahtee Boo on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreData1of7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
